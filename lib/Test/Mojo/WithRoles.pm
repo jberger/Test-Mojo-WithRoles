2c6b2c427fecbd6fe36c00670f9d69b8da6868f4
Joel Berger <joel.a.berger@gmail.com>
2015-02-28 13:06:15 -0600

    simplify hint hash logic significantly

--- lib/Test/Mojo/WithRoles.pm
+++ lib/Test/Mojo/WithRoles.pm
@@ -10,25 +10,20 @@ use Mojo::JSON 'j';
 sub import {
   my ($class, @roles) = @_;
   @roles = map { s/^\+// ? $_ : "Test::Mojo::Role::$_" } @roles;
-  my $stack = j($^H{'Test::Mojo::WithRoles/stack'} || '[]');
-  push @$stack, \@roles;
-  $^H{'Test::Mojo::WithRoles/stack'} = j($stack);
+  $^H{'Test::Mojo::WithRoles/enabled'} = j(\@roles);
 }
 
 sub unimport {
-  my ($class, @roles) = @_;
-  my $stack = j($^H{'Test::Mojo::WithRoles/stack'} || '[]');
-  pop @$stack;
-  $^H{'Test::Mojo::WithRoles/stack'} = j($stack);
+  my ($class) = @_;
+  $^H{'Test::Mojo::WithRoles/enabled'} = j([]);
 }
 
 sub new {
   my $class = shift;
   my $hints = (caller(0))[10];
-  my $stack = j($hints->{'Test::Mojo::WithRoles/stack'} || '[]');
-  my @roles = @{ $stack->[-1] || [] };
-  @roles = 'Test::Mojo::Role::Null' unless @roles;
-  return Role::Tiny->create_class_with_roles('Test::Mojo', @roles)->new(@_);
+  my $roles = j($hints->{'Test::Mojo::WithRoles/enabled'} || '[]');
+  @$roles = 'Test::Mojo::Role::Null' unless @$roles;
+  return Role::Tiny->create_class_with_roles('Test::Mojo', @$roles)->new(@_);
 }
 
 1;
